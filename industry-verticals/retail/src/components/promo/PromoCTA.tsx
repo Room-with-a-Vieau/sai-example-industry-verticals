a69b08b8167629b770e8c381fcf4a4c1437dbff7
Andrew <andrew.vieau@gmail.com>
2026-03-06 23:53:09 -0600

    ugh

--- industry-verticals/retail/src/components/promo/PromoCTA.tsx
+++ industry-verticals/retail/src/components/promo/PromoCTA.tsx
@@ -10,12 +10,9 @@ import {
   Text,
 } from '@sitecore-content-sdk/nextjs';
 import { ComponentProps } from 'lib/component-props';
-import { isParamEnabled } from '@/helpers/isParamEnabled';
 
 interface Fields {
   PromoImageOne: ImageField;
-  PromoImageTwo: ImageField;
-  PromoImageThree: ImageField;
   PromoTitle: Field<string>;
   PromoDescription: RichTextField;
   PromoSubTitle: Field<string>;
@@ -28,36 +25,26 @@ export type PromoProps = ComponentProps & {
 
 export const Default = (props: PromoProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
-  const isPromoReversed = !isParamEnabled(props.params.Reversed) ? '' : 'order-last';
 
   return (
-    <section className={`${props.params.styles} py-20`} id={id ? id : undefined}>
-      <div className="container grid grid-cols-1 place-items-center gap-10 lg:grid-cols-2">
-        <div className={`${isPromoReversed} relative w-full`}>
-          <div
-            className={`relative z-10 aspect-4/3 w-full max-w-4xl overflow-hidden rounded-2xl shadow-2xl`}
-          >
-            <ContentSdkImage
-              field={props.fields.PromoImageOne}
-              className="h-full w-full object-cover"
-            />
-          </div>
+    <section className={props.params.styles} id={id ? id : undefined}>
+      <div>
+        <div>
+          <ContentSdkImage field={props.fields.PromoImageOne} />
         </div>
 
-        <div className="space-y-5">
-          <div className="eyebrow">
+        <div>
+          <div>
             <Text field={props.fields.PromoSubTitle} />
           </div>
 
-          <h2 className="inline-block max-w-md">
+          <h2>
             <Text field={props.fields.PromoTitle} />
           </h2>
 
-          <div className="max-w-lg text-lg">
-            <ContentSdkRichText field={props.fields.PromoDescription} />
-          </div>
+          <ContentSdkRichText field={props.fields.PromoDescription} />
 
-          <Link field={props.fields.PromoMoreInfo} className="main-btn" />
+          <Link field={props.fields.PromoMoreInfo} />
         </div>
       </div>
     </section>
